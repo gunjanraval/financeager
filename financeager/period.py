gunjanraval/financeager

commit af1f02b94972cf9f2869afce2faced7992b8e942

Make database a member of TinyDbPeriod instead of subclassing.

financeager/period.py
@@ -63,7 +63,9 @@ class PeriodException(Exception):
     pass
 
 
-class TinyDbPeriod(TinyDB, Period):
+class TinyDbPeriod(Period):
+
+    DEFAULT_TABLE = "standard"
 
     def __init__(self, name=None, *args, **kwargs):
         """
@@ -72,11 +74,12 @@ class TinyDbPeriod(TinyDB, Period):
         Keyword args are passed to the TinyDB constructor (f.i. storage type).
         """
 
-        self._name = "{}".format(Period.DEFAULT_NAME if name is None else name)
+        super().__init__(name=name)
+
         if kwargs.get("storage", JSONStorage) == JSONStorage:
-            # TODO: find a nicer way around this
             args = list(args) + [os.path.join(CONFIG_DIR, "{}.json".format(self._name))]
-        super(TinyDbPeriod, self).__init__(*args, **kwargs)
+
+        self._db = TinyDB(*args, **kwargs)
         self._create_category_cache()
 
     def _create_category_cache(self):
@@ -85,7 +88,7 @@ class TinyDbPeriod(TinyDB, Period):
         categories the element was labeled with. This allows deriving the
         category of an element if not explicitly given."""
         self._category_cache = defaultdict(Counter)
-        for element in self.all():
+        for element in self._db.all():
             self._category_cache[element["name"]].update([element["category"]])
 
     def _preprocess_entry(self, raw_data=None, table_name=None, partial=False):
@@ -269,12 +272,12 @@ class TinyDbPeriod(TinyDB, Period):
         :return: TinyDB ID of new entry (int)
         """
 
-        table_name = table_name or self.DEFAULT_TABLE
+        table_name = table_name or TinyDbPeriod.DEFAULT_TABLE
         fields = self._preprocess_entry(raw_data=kwargs, table_name=table_name)
 
         self._update_category_cache(**fields)
 
-        element_id = self.table(table_name).insert(fields)
+        element_id = self._db.table(table_name).insert(fields)
 
         return element_id
 
@@ -292,7 +295,8 @@ class TinyDbPeriod(TinyDB, Period):
         if eid is None:
             raise PeriodException("No element ID specified.")
 
-        element = self.table(table_name or TinyDB.DEFAULT_TABLE).get(eid=int(eid))
+        table_name = table_name or TinyDbPeriod.DEFAULT_TABLE
+        element = self._db.table(table_name).get(eid=int(eid))
         if element is None:
             raise PeriodException("Element not found.")
 
@@ -314,13 +318,14 @@ class TinyDbPeriod(TinyDB, Period):
         if eid is None:
             raise PeriodException("No element ID specified.")
 
-        table_name = table_name or TinyDB.DEFAULT_TABLE
+        table_name = table_name or TinyDbPeriod.DEFAULT_TABLE
         fields = self._preprocess_entry(raw_data=kwargs, table_name=table_name,
                 partial=True)
 
         self._update_category_cache(eid=eid, table_name=table_name, **fields)
 
-        element_id = self.table(table_name).update(fields, eids=[int(eid)])[0]
+        element_id = self._db.table(table_name).update(
+            fields, eids=[int(eid)])[0]
 
         return element_id
 
@@ -346,9 +351,9 @@ class TinyDbPeriod(TinyDB, Period):
                 }
 
         if query_impl is None:
-            matching_standard_elements = self.all()
+            matching_standard_elements = self._db.all()
         else:
-            matching_standard_elements = self.search(query_impl)
+            matching_standard_elements = self._db.search(query_impl)
 
         for element in matching_standard_elements:
             elements["standard"][element.eid] = element
@@ -356,7 +361,7 @@ class TinyDbPeriod(TinyDB, Period):
         # all recurrent elements are generated, and the ones matching the
         # query are appended to a list that is stored under their generating
         # element's eid in the 'recurrent' subdictionary
-        for element in self.table("recurrent").all():
+        for element in self._db.table("recurrent").all():
             for e in self._create_recurrent_elements(element):
                 matching_recurrent_element = None
 
@@ -432,11 +437,11 @@ class TinyDbPeriod(TinyDB, Period):
         if eid is None:
             raise PeriodException("No element ID specified.")
 
-        table_name = table_name or TinyDB.DEFAULT_TABLE
+        table_name = table_name or TinyDbPeriod.DEFAULT_TABLE
         # might raise PeriodException if ID not existing
         entry = self.get_entry(eid=int(eid), table_name=table_name)
 
-        self.table(table_name).remove(eids=[entry.eid])
+        self._db.table(table_name).remove(eids=[entry.eid])
         self._update_category_cache(removing=True, **entry)
 
         return entry.eid
@@ -481,4 +486,4 @@ class TinyDbPeriod(TinyDB, Period):
         return self._search_all_tables(condition)
 
 
-TinyDB.DEFAULT_TABLE = "standard"
+TinyDB.DEFAULT_TABLE = TinyDbPeriod.DEFAULT_TABLE

test/test_period.py
@@ -87,7 +87,7 @@ class TinyDbPeriodStandardEntryTestCase(unittest.TestCase):
 
     def test_remove_entry(self):
         response = self.period.remove_entry(eid=1)
-        self.assertEqual(0, len(self.period))
+        self.assertEqual(0, len(self.period._db))
         self.assertEqual(1, response)
 
     def test_create_models_query_kwargs(self):
@@ -125,11 +125,11 @@ class TinyDbPeriodStandardEntryTestCase(unittest.TestCase):
         entry_name = "penguin sale"
         entry_id = self.period.add_entry(name=entry_name, value=1337,
                 date="1901-12-01")
-        nr_entries = len(self.period)
+        nr_entries = len(self.period._db)
 
         removed_entry_id = self.period.remove_entry(eid=entry_id)
         self.assertEqual(removed_entry_id, entry_id)
-        self.assertEqual(len(self.period), nr_entries - 1)
+        self.assertEqual(len(self.period._db), nr_entries - 1)
         self.assertEqual(self.period._category_cache[entry_name]["unspecified"], 0)
 
     def test_get_nonexisting_entry(self):
@@ -190,7 +190,7 @@ class TinyDbPeriodStandardEntryTestCase(unittest.TestCase):
                 value="hundred")
 
     def tearDown(self):
-        self.period.close()
+        self.period._db.close()
 
 
 class TinyDbPeriodRecurrentEntryNowTestCase(unittest.TestCase):
@@ -217,10 +217,10 @@ class TinyDbPeriodRecurrentEntryTestCase(unittest.TestCase):
     def test_recurrent_entries(self):
         eid = self.period.add_entry(name="rent", value=-500,
                 table_name="recurrent", frequency="monthly", start="10-01")
-        self.assertSetEqual({"standard", "recurrent"}, self.period.tables())
+        self.assertSetEqual({"standard", "recurrent"}, self.period._db.tables())
 
-        self.assertEqual(len(self.period.table("recurrent").all()), 1)
-        element = self.period.table("recurrent").all()[0]
+        self.assertEqual(len(self.period._db.table("recurrent").all()), 1)
+        element = self.period._db.table("recurrent").all()[0]
         recurrent_elements = list(self.period._create_recurrent_elements(element))
         self.assertEqual(len(recurrent_elements), 3)
 
@@ -244,7 +244,7 @@ class TinyDbPeriodRecurrentEntryTestCase(unittest.TestCase):
                 table_name="recurrent", frequency="quarter-yearly",
                 start="01-01")
 
-        element = self.period.table("recurrent").all()[0]
+        element = self.period._db.table("recurrent").all()[0]
         recurrent_elements = list(self.period._create_recurrent_elements(element))
         self.assertEqual(len(recurrent_elements), 4)
 
@@ -253,9 +253,9 @@ class TinyDbPeriodRecurrentEntryTestCase(unittest.TestCase):
                 {"interest, january", "interest, april", "interest, july",
                     "interest, october"})
 
-        recurrent_table_size = len(self.period.table("recurrent"))
+        recurrent_table_size = len(self.period._db.table("recurrent"))
         self.period.remove_entry(eid=eid, table_name="recurrent")
-        self.assertEqual(len(self.period.table("recurrent")),
+        self.assertEqual(len(self.period._db.table("recurrent")),
                 recurrent_table_size - 1)
 
     def test_recurrent_weekly_entries(self):
@@ -294,7 +294,7 @@ class TinyDbPeriodRecurrentEntryTestCase(unittest.TestCase):
         self.assertIn("end", str(context.exception))
 
     def tearDown(self):
-        self.period.close()
+        self.period._db.close()
 
 
 class ValidationModelTestCase(unittest.TestCase):

commit 22843adc4ab322ed88dedf4d87b9532c2d772903

Add missing TinyDbPeriod method
Old bug (introduced in af1f02b) but not discovered because of the broad
exception handling in Server.run(); and because the response of
run('stop') in LocalServer.run was not checked

financeager/period.py
@@ -484,5 +484,9 @@ class TinyDbPeriod(Period):
         condition = self._create_query_condition(**filters)
         return self._search_all_tables(condition)
 
+    def close(self):
+        """Close underlying database."""
+        self._db.close()
+
 
 TinyDB.DEFAULT_TABLE = TinyDbPeriod.DEFAULT_TABLE